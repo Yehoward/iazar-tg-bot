357bbaecbd1d299d304a323d8bcbaa0210b8cf2d
YeRudIT <rudencoyehoward@gmail.com>
2024-05-01 18:20:08 +0300

    Botul de acum suportă noi tipuri de fișiere, inclusiv video

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from os import environ
+from os import environ, remove
+import subprocess
 import sys
 import asyncio
+from tempfile import NamedTemporaryFile
+from subprocess import Popen
 
-from telebot import TeleBot, types as T
-from telebot.async_telebot import AsyncTeleBot
-from telebot.util import quick_markup
+from telebot.async_telebot import AsyncTeleBot, types as T
 
 from transformers import pipeline
 
@@ -16,7 +17,7 @@ bot = AsyncTeleBot(TOKEN)
 
 pipe = pipeline(model="Yehoward/whisper-small-ro")
 
-def transcribe(audio: bytes ) -> str:
+def transcribe(audio: bytes | str) -> str:
     return pipe(audio)["text"]
 
 def is_voice(msg: T.Message) -> bool:
@@ -24,19 +25,67 @@ def is_voice(msg: T.Message) -> bool:
 
     
 
-@bot.message_handler( content_types=["voice"])
+@bot.message_handler( content_types=["voice", "audio"])
 async def transcribe_voice(msg: T.Message):
 
     print("am primit mesaj vocal")
-    if msg.voice is None:
+
+    match msg.content_type:
+        case "voice":
+            msg_content = msg.voice
+        case "audio":
+            msg_content = msg.audio
+        case _:
+            print("transcribe_voice: wrogn content type", file=sys.stderr)
+            return
+
+    if msg_content is None:
         print("transcribe_voice: Telebot bug", file=sys.stderr)
         return 
 
     raspuns = await bot.reply_to(msg, "descarcăm audioul")
-    file: T.File = await bot.get_file(msg.voice.file_id)
-    data = await bot.download_file(file.file_path)
+    try:
+        file: T.File = await bot.get_file(msg_content.file_id)
+        data = await bot.download_file(file.file_path)
+    except Exception as e:
+        print(f"{type(e)} : {e}")
+        await bot.send_message(msg.chat.id,"Fișierul este mai mare de 20mb")
+        return
 
     text = transcribe(data)
+
+    await bot.edit_message_text(text, msg.chat.id, raspuns.id)
+
+@bot.message_handler( content_types=["video"])
+async def transcribe_video(msg: T.Message):
+
+    print("am primit mesaj video")
+
+    msg_content = msg.video
+    
+    if msg_content is None:
+        print("transcribe_video: Telebot bug", file=sys.stderr)
+        return 
+
+
+    raspuns = await bot.reply_to(msg, "descarcăm videoul")
+    try:
+        file: T.File = await bot.get_file(msg_content.file_id)
+        data = await bot.download_file(file.file_path)
+    except Exception as e:
+        print(f"{type(e)} : {e}")
+        await bot.send_message(msg.chat.id,"Fișierul este mai mare de 20mb")
+        return
+
+    with NamedTemporaryFile("wb+", suffix=".wav") as f:
+        f.flush()
+
+    ffmpeg_cmd: list =  f"ffmpeg -i - {f.name}".split()
+    with Popen(ffmpeg_cmd, stdin=subprocess.PIPE) as proc:
+        proc.communicate(input=data)
+    text = transcribe(f.name)
+
+    remove(f.name)
 
     await bot.edit_message_text(text, msg.chat.id, raspuns.id)
 
